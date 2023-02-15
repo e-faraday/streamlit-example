531d04eaa1081f44c782166d9ec0535ed71d6521
e-faraday <55401664+e-faraday@users.noreply.github.com>
2023-02-15 18:12:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,7 @@ def run_query(query):
     with conn.cursor() as cur:
         cur.execute(query)
         return cur.fetchall()
-    
- rows = run_query("SELECT * from testschema15.test_table15;")   
+rows = run_query("SELECT * from testschema15.test_table15;")   
 
 for row in rows:
     st.write(f"{row[0]} has a :{row[1]}:")
